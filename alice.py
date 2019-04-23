fb05e03098151c2ef16a270cd8a32ad8a9e8f8c5
malinroot <kurchevskijpavel@gmail.com>
2019-04-23 16:20:59 +0300

    Add files via upload

--- alice.py
+++ alice.py
@@ -1,4 +1,5 @@
-
+"""Does not display error tracing.
+And repsorts if the file does not exist."""
 filename = 'alice.txt'
 
 try:
@@ -8,7 +9,7 @@ except FileNotFoundError:
     msg = "Sorry, the file " + filename + " does not exist."
     print(msg)
 else:
-    #Count the apperoximate number of lines in the file.
+    # Count the apperoximate number of lines in the file.
     words = contents.split()
     num_words = len(words)
     print("The file " + filename + " has about " + str(num_words) + 
