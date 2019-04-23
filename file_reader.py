24ec7daedbb777b71da4af9fd8b0c06c18cd0226
malinroot <kurchevskijpavel@gmail.com>
2019-04-23 16:03:18 +0300

    Add files via upload

--- file_reader.py
+++ file_reader.py
@@ -1,22 +1,24 @@
-
+# Reading, removing indents
 with open('pi_digits.txt') as file_object:
     contents = file_object.read()
     print(contents)
     print(contents.rstrip())
 
+# Reading, output one by one
 filename = 'pi_digits.txt'
 
 with open(filename) as file_object:
     for line in file_object:
         print(line)
 
+# Reading, outputting one by one and removing gaps from the right edge
 filename = 'pi_digits.txt'
 
 with open(filename) as file_object:
     for line in file_object:
         print(line.rstrip())
 
-
+# Reading outputting one by one and removing gaps from the right edge
 filename = 'pi_digits.txt'
 
 with open('pi_digits.txt') as file_object:
